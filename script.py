5c58bd5b094e0529054bf6359a9ccfe2f69b0a9a
naseef1235 <80143146+naseef1235@users.noreply.github.com>
2021-05-31 11:41:57 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,10 +1,10 @@
 class script(object):
 
 
-    START_MSG = """ <b>Sorry {}, This Bot Can Only Be Used In @CFCHART ..!</b>"""
+    START_MSG = """ <b>Sorry {}, This Bot is property of @MKCHANEL1</b>"""
 
 
     HELP_MSG = """<b>Sorry, I Can't Help You!</b>"""
 
 
-    ABOUT_MSG = """⭕️<b>Nothing Great About Me..!</b>"""
+    ABOUT_MSG = """<b>Nothing Great About Me..!</b>"""
